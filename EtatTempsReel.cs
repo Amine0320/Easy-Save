313d54fc6fa3e7449ca0591b930c5f83b7e187db
Bruno COLQUE ALARCON <bruno.colquealarcon@viacesi.fr>
2023-11-29 10:23:48 +0000

    Added 11 files to /

---
+++ EtatTempsReel.cs
@@ -0,0 +1,17 @@
+﻿using System.Management.Automation;
+using System.Text;
+
+namespace PowershellShowcase;
+
+public class EtatTempsReel
+{
+    private int IdEtatTemp;
+    public string NomETR;
+    public string SourceFilePath;
+    public string TargetFilePath;
+    public string State;
+    public int TotalFilesToCopy;
+    public int TotalFilesSize;
+    public int NbFilesLeftToDo;
+    public int Progression;
+}
