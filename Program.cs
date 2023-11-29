b49fbcd62c9eb8f74a2170b5466430d0a9c7e9e1
sarahkom <150901223+sarahkom@users.noreply.github.com>
2023-11-29 13:12:08 +0100

    Amélioration de l'affichage et ajout de la condition 0<i<5

--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 using System.ComponentModel;
 using System.Linq;
 
+
 static void Main()
 {
     Console.WriteLine("************************");
     Console.WriteLine("***Project Easy Save ***");
     Console.WriteLine("************************");
     Console.WriteLine("Choisissez les sauvegardes à effectuer (sous format '1-3' ou '1;3')") ;
-    Console.WriteLine("Choose which saves to execute (using the template  '1 - 3' ou '1; 3')") ;
+    Console.WriteLine("Choose which saves to execute (using the template  '1-3' or '1; 3')") ;
     string saves = Console.ReadLine();
     List<int> listeDeSauvegardes = ListeDeSauv(saves);
     Systeme TravailNouvelle = new Systeme();
@@ -20,16 +21,22 @@ static void Main()
     {
         foreach (int i in listeDeSauvegardes)
         {
+            Console.WriteLine("");
+            Console.WriteLine("Entrez les paramètres de la sauvegarde " + i.ToString());
+            Console.WriteLine("Enter the parametres of save " + i.ToString());
+            Console.WriteLine("");
             Console.WriteLine("Où sont les fichiers à savegarder?");
             Console.WriteLine("Where are the files to save?");
             string Sources = Console.ReadLine();
+            Console.WriteLine("");
             Console.WriteLine("Où sauvegarder les fichiers?");
             Console.WriteLine("Where should the files be saved?");
             string Cible = Console.ReadLine();
+            Console.WriteLine("");
             Console.WriteLine("Quel type de sauvegarde (entre 'complete' et 'differentiel')");
             Console.WriteLine("Which type of save (either 'complete' or 'differentiel')");
             string Type = Console.ReadLine();
-            TravailNouvelle.EnregistrerSauvegarde(TravailNouvelle.CreerSauvegarde(i, Sources, Cible, Type));
+            //TravailNouvelle.EnregistrerSauvegarde(TravailNouvelle.CreerSauvegarde(i, Sources, Cible, Type));
             Systeme.SauvDejaCreee.Add(i);
         }
     }
@@ -54,3 +61,5 @@ static List<int> ListeDeSauv(string sauv)
     }
     return listeSauv;
 }
+
+Main();
