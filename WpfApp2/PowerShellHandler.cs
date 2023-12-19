5b5b503de8dfa645548abd899f92f84bddf8cd21
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2023-12-19 23:26:45 +0100

    PowerShellHandler Commented

--- WpfApp2/PowerShellHandler.cs
+++ WpfApp2/PowerShellHandler.cs
@@ -1,49 +1,63 @@
 ﻿using System.Management.Automation;
 using System.Text;
 using System;
-namespace PowershellShowcase;
 
-public static class PowerShellHandler
+namespace PowershellShowcase
 {
-    private static readonly PowerShell _ps = PowerShell.Create();
-
-    public static string Command(string script)
+    /// <summary>
+    /// A utility class for handling PowerShell commands.
+    /// </summary>
+    public static class PowerShellHandler
     {
-        string errorMsg = string.Empty;
+        private static readonly PowerShell _ps = PowerShell.Create();
 
-        _ps.AddScript(script);
-
-        //Make sure return values are outputted to the stream captured by C#
-        _ps.AddCommand("Out-String");
-
-        PSDataCollection<PSObject> outputCollection = new();
-        _ps.Streams.Error.DataAdded += (object sender, DataAddedEventArgs e) =>
+        /// <summary>
+        /// Executes a PowerShell command or script and returns the output as a string.
+        /// </summary>
+        /// <param name="script">The PowerShell script or command to execute.</param>
+        /// <returns>The output of the PowerShell script or command as a string.</returns>
+        public static string Command(string script)
         {
-            errorMsg = ((PSDataCollection<ErrorRecord>)sender)[e.Index].ToString();
-        };
+            string errorMsg = string.Empty;
 
+            // Add the script to the PowerShell session
+            _ps.AddScript(script);
 
-        IAsyncResult result = _ps.BeginInvoke<PSObject, PSObject>(null, outputCollection);
+            // Make sure return values are outputted to the stream captured by C#
+            _ps.AddCommand("Out-String");
 
-        //Wait for powershell command/script to finish executing
-        _ps.EndInvoke(result);
+            // Set up a data collection for PowerShell output
+            PSDataCollection<PSObject> outputCollection = new();
 
-        StringBuilder sb = new();
+            // Event handler for capturing PowerShell errors
+            _ps.Streams.Error.DataAdded += (object sender, DataAddedEventArgs e) =>
+            {
+                errorMsg = ((PSDataCollection<ErrorRecord>)sender)[e.Index].ToString();
+            };
 
-        foreach (var outputItem in outputCollection)
-        {
-            sb.AppendLine(outputItem.BaseObject.ToString());
-        } 
-        
-        //Clears the commands we added to the powershell runspace so it's empty the next time we use it
-        _ps.Commands.Clear();
+            // Begin asynchronous invocation of PowerShell script or command
+            IAsyncResult result = _ps.BeginInvoke<PSObject, PSObject>(null, outputCollection);
 
-        //If an error is encountered, return it
-        if (!string.IsNullOrEmpty(errorMsg))
-            return errorMsg;
+            // Wait for PowerShell command/script to finish executing
+            _ps.EndInvoke(result);
 
-        return sb.ToString().Trim();
-    
+            StringBuilder sb = new();
+
+            // Append each output item to the StringBuilder
+            foreach (var outputItem in outputCollection)
+            {
+                sb.AppendLine(outputItem.BaseObject.ToString());
+            }
+
+            // Clears the commands in the PowerShell runspace for the next use
+            _ps.Commands.Clear();
+
+            // If an error is encountered, return it
+            if (!string.IsNullOrEmpty(errorMsg))
+                return errorMsg;
+
+            return sb.ToString().Trim();
+        }
     }
-
 }
+ 
