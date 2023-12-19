2555d98bd333ba7c1af8be621bc69f20cab06eb1
Elbah Mohamed Amine <elbah.ma02@gmail.com>
2023-12-19 23:09:39 +0100

    EtatTempsReel Commented

--- WpfApp2/EtatTempsReel.cs
+++ WpfApp2/EtatTempsReel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -7,11 +6,14 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace WpfApp2
+namespace WpfApp2 
 {
+    /// <summary>
+    /// Represents the real-time status of a backup operation.
+    /// </summary>
     public class EtatTempsReel
     {
-        // Declaration des attribus 
+        // Declaration of attributes
         public int IdEtaTemp { get; set; }
         public string NomETR { get; set; }
         public string SourceFilePath { get; set; }
@@ -22,62 +24,23 @@ namespace WpfApp2
         public int NbFilesLeftToDo { get; set; }
         public int Progression { get; set; }
 
-        
-        
+        /// <summary>
+        /// Saves the real-time status to a JSON file using a PowerShell script.
+        /// </summary>
+        /// <param name="Source">The source path.</param>
+        /// <param name="Target">The target path.</param>
+        /// <param name="iden">The identifier.</param>
+        /// <param name="cancellationTokenSource">The cancellation token source.</param>
         public async void SaveToJson(string Source, string Target, int iden, CancellationTokenSource cancellationTokenSource)
         {
-            //cancellationTokenSource = new CancellationTokenSource();
             try
             {
                 await Task.Run(() =>
-            {
-                string rutaScriptPowerShell = GlobalVariables.Dir + "ExecuteLogTempReel.ps1";
-                ProcessStartInfo psi = new ProcessStartInfo
                 {
-                    FileName = "powershell.exe",
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true, 
-                    CreateNoWindow = true,
-                    UseShellExecute = false
-                };
-                //Console.WriteLine("***Ejecutando ***");
-                using (Process proceso = new Process { StartInfo = psi })
-                {
-                    proceso.Start();
+                    // Define the path to the PowerShell script
+                    string rutaScriptPowerShell = GlobalVariables.Dir + "ExecuteLogTempReel.ps1";
 
-                    // Cargar y ejecutar el script PowerShell
-                    using (System.IO.StreamWriter sw = proceso.StandardInput)
-                    {
-                        if (sw.BaseStream.CanWrite)
-                        {
-                            sw.WriteLine($"& '{rutaScriptPowerShell}' -sourcePath " + Source + " -destinationPath " + Target + " -ID " + iden);
-                        }
-                    }
-                    //cancellationTokenSource.Cancel();
-                    // Obtener la salida del proceso
-                    //string resultado = proceso.StandardOutput.ReadToEnd();
-
-                    // Mostrar la salida (puedes hacer otras cosas con el resultado)
-                    //Console.WriteLine("Resultado:");
-                    //Console.WriteLine(resultado);
-                }
-            }, cancellationTokenSource.Token);
-                
-            } catch (OperationCanceledException)
-            {
-                throw;
-            }
-            
-            return;
-        }
-        public async void SaveToJsonDiff(string Source, string Target, int iden, CancellationTokenSource cancellationTokenSource)
-        {
-            //Console.WriteLine("***En proceso ***");
-            try {
-                await Task.Run(() =>
-                {
-                    string rutaScriptPowerShell = GlobalVariables.Dir + "ExecuteLogTempReelDiff.ps1";
+                    // Configure the process start info for the PowerShell execution
                     ProcessStartInfo psi = new ProcessStartInfo
                     {
                         FileName = "powershell.exe",
@@ -87,12 +50,13 @@ namespace WpfApp2
                         CreateNoWindow = true,
                         UseShellExecute = false
                     };
-                    //Console.WriteLine("***Ejecutando ***");
+
+                    // Start a new process for executing the PowerShell script
                     using (Process proceso = new Process { StartInfo = psi })
                     {
                         proceso.Start();
 
-                        // Cargar y ejecutar el script PowerShell
+                        // Load and execute the PowerShell script
                         using (System.IO.StreamWriter sw = proceso.StandardInput)
                         {
                             if (sw.BaseStream.CanWrite)
@@ -100,26 +64,69 @@ namespace WpfApp2
                                 sw.WriteLine($"& '{rutaScriptPowerShell}' -sourcePath " + Source + " -destinationPath " + Target + " -ID " + iden);
                             }
                         }
-
-                        // Obtener la salida del proceso
-                        //string resultado = proceso.StandardOutput.ReadToEnd();
-
-                        // Mostrar la salida (puedes hacer otras cosas con el resultado)
-                        //Console.WriteLine("Resultado:");
-                        //Console.WriteLine(resultado);
                     }
                 }, cancellationTokenSource.Token);
+
+            }
+            catch (OperationCanceledException)
+            {
+                throw;
+            }
+
+            return;
+        }
+
+        /// <summary>
+        /// Saves the real-time status for differential backup to a JSON file using a PowerShell script.
+        /// </summary>
+        /// <param name="Source">The source path.</param>
+        /// <param name="Target">The target path.</param>
+        /// <param name="iden">The identifier.</param>
+        /// <param name="cancellationTokenSource">The cancellation token source.</param>
+        public async void SaveToJsonDiff(string Source, string Target, int iden, CancellationTokenSource cancellationTokenSource)
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    // Define the path to the PowerShell script for differential backup
+                    string rutaScriptPowerShell = GlobalVariables.Dir + "ExecuteLogTempReelDiff.ps1";
+
+                    // Configure the process start info for the PowerShell execution
+                    ProcessStartInfo psi = new ProcessStartInfo
+                    {
+                        FileName = "powershell.exe",
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        CreateNoWindow = true,
+                        UseShellExecute = false
+                    };
+
+                    // Start a new process for executing the PowerShell script
+                    using (Process proceso = new Process { StartInfo = psi })
+                    {
+                        proceso.Start();
+
+                        // Load and execute the PowerShell script
+                        using (System.IO.StreamWriter sw = proceso.StandardInput)
+                        {
+                            if (sw.BaseStream.CanWrite)
+                            {
+                                sw.WriteLine($"& '{rutaScriptPowerShell}' -sourcePath " + Source + " -destinationPath " + Target + " -ID " + iden);
+                            }
+                        }
+                    }
+                }, cancellationTokenSource.Token);
+
                 cancellationTokenSource.Token.ThrowIfCancellationRequested();
             }
             catch (OperationCanceledException)
             {
                 throw;
             }
-            
-            
+
             return;
         }
-        
     }
-
-}
+} 
