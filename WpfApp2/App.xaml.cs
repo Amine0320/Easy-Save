9439803c6d3eda6aedddc7a6dd301dc6d431b4ce
sarahkom <150901223+sarahkom@users.noreply.github.com>
2023-12-21 17:08:33 +0100

    MonoInstance

--- WpfApp2/App.xaml.cs
+++ WpfApp2/App.xaml.cs
@@ -9,7 +9,19 @@ namespace WpfApp2
     /// </summary>
     public partial class App : Application
     {
-      
-    }
+        private static Mutex mutex = new Mutex(true, "{1E8C4D6A-9C5C-4A0E-9D85-0E26F55D7E2F}");
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                base.OnStartup(e);
+            }
+            else
+            {
+                MessageBox.Show("L'application est déjà en cours d'exécution.", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                Shutdown();
+            }
+        }
+    }
 }
