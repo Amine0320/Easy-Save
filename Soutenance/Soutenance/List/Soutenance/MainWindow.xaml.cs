4ddfacc5be8cd352f37fadebb1e29abfe256afa6
Bruno COLQUE ALARCON <bruno.colquealarcon@viacesi.fr>
2023-12-21 19:27:44 +0100

    VALIDATETOUT

---
+++ Soutenance/Soutenance/List/Soutenance/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+using System.IO;
+using System.Windows;
+using System.Windows.Markup;
+using System.Reflection.Metadata;
+//using WpfApp2;
+
+namespace Soutenance
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (TcpClient client = new TcpClient("127.0.0.1", 12345))
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.UTF8.GetBytes("OpenWPFPage");
+                stream.Write(data, 0, data.Length);
+                //byte[] message = new byte[1024];
+                //int bytesRead = stream.Read(data, 0, 1024);
+                //string request = Encoding.UTF8.GetString(message, 0, bytesRead);
+                //byte[] data = Encoding.UTF8.GetBytes(request);
+                //WpfApp6.MainWindow newForm;
+                Window1 window1 = new Window1();
+                window1.Show();
+                this.Close();
+                //Window ventanaDesdeXaml = (Window)XamlReader.Load(new MemoryStream(Encoding.UTF8.GetBytes(request)));
+            }
+        }
+    }
+}
